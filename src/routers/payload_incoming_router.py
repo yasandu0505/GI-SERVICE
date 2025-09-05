674d528c32cce70dfde0979575ce04ff06bc300b
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-09-05 15:35:28 +0530

    feat : implement data extracting service from the req

--- src/routers/payload_incoming_router.py
+++ src/routers/payload_incoming_router.py
@@ -7,4 +7,6 @@ service = IncomingService()
 
 @router.post("/data/{ministryId}")
 async def get_data(REQ_ONE: REQ_ONE , ministryId : str):
-    return service.incoming_payload_extractor(REQ_ONE , ministryId)
+    extracted_data = service.incoming_payload_extractor(REQ_ONE , ministryId)
+    return service.query_aggregator(extracted_data)
+
--- src/services/payload_incoming.py
+++ src/services/payload_incoming.py
@@ -2,5 +2,30 @@ from src.models import REQ_ONE
 
 class IncomingService:
     def incoming_payload_extractor(self, REQ_ONE: REQ_ONE , ministryId ):
-        return {"dataReq": REQ_ONE , "ministryId" : ministryId}
+        year = REQ_ONE.year
+        year = REQ_ONE.year
+        govId = REQ_ONE.govId
+        presidentId = REQ_ONE.presidentId
+        dataSet = REQ_ONE.dataSet 
+            
+        return {
+            "year" : year,
+            "govId" : govId,
+            "presidentId" : presidentId,
+            "dataSet" : dataSet,
+            "ministryId" : ministryId
+        }
+    
+    def query_aggregator(self, extracted_data):
+        print(extracted_data)
+        
+    
+        return {
+            "extracted_data" : extracted_data
+        }
+    
+    
+    
+    
+    
         
