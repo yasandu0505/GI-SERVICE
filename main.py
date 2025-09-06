3785228e364a3151226459f227ef674e8204e310
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-09-06 20:24:05 +0530

    set : set the metadata for the doc of the project

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from src.routers import payload_incoming_router
 
-app = FastAPI()
+app = FastAPI(
+    title="GI - Service",     
+    description="API Adapter to the OpenGIn (Open Genaral Information Network)",
+    version="1.0.0"
+)
 
 app.include_router(payload_incoming_router.router)
