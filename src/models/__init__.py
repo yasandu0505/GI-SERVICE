2e3bc3ac833175aff3dc9d4ef81cf1a1d1bd2187
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-09-21 14:57:03 +0530

    feat : adding the write payload

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,9 @@
 from .entity_payload_incoming import ENTITY_PAYLOAD
 from .attribute_payload_incoming import ATTRIBUTE_PAYLOAD
+from .write_payload import WRITE_PAYLOAD
 
 __all__ = [
     "ENTITY_PAYLOAD",
-    "ATTRIBUTE_PAYLOAD"
+    "ATTRIBUTE_PAYLOAD",
+    "WRITE_PAYLOAD"
 ]
---
+++ src/models/write_payload.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+
+class WRITE_PAYLOAD(BaseModel):
+    base_url : str
