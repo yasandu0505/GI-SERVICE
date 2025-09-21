7572b03470de29d337c27d07d5b9cd7a88ffe27d
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-09-21 15:00:31 +0530

    delete : remove cache program

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 from src.routers import payload_incoming_router
-from src.utils import prepare_cache, CacheService
-import redis.asyncio as redis
 # import json
 import yaml
 
@@ -16,32 +14,6 @@ app = FastAPI(
 async def load_config():
     with open("config.yaml", "r") as f:
         app.state.config = yaml.safe_load(f)
-
-
-# @app.on_event("startup")
-# async def startup_event():
-#     print("🚀 App starting... connecting to Redis")
-
-#     app.state.redis = redis.from_url("redis://localhost:6379", decode_responses=True)
-    
-#     ministries = await prepare_cache("Organisation", "minister")
-#     print("✅ ministries preloaded into Redis")
-#     departments = await prepare_cache("Organisation", "department")
-#     print("✅ departments preloaded into Redis")
-#     people = await prepare_cache("Person", "citizen")
-#     print("✅ people preloaded into Redis")
-    
-#     await app.state.redis.set("ministries", json.dumps(ministries))
-#     await app.state.redis.set("departments", json.dumps(departments))
-#     await app.state.redis.set("people", json.dumps(people))
-    
-#     print("✅ Cache preloaded into Redis - Complete")
-
-# @app.on_event("shutdown")
-# async def shutdown_event():
-#     await app.state.redis.close()
-#     print("🛑 Redis connection closed")
-
 
     
 app.include_router(payload_incoming_router.router)
