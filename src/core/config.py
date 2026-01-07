c003c7b45fe68e660c69ffb6d063b2fcb51acbe6
ChanukaUOJ <chamithroxmpc@gmail.com>
2026-01-07 11:30:16 +0530

    default base url removed in the pydantic settings

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    BASE_URL_QUERY: str = "http://localhost:8081"
+    BASE_URL_QUERY: str
     BASE_URL_CRUD: str | None = None
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
