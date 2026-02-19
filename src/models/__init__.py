aaf4490be1956fb7a775f8e3e1f86ad96a76ad20
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-02-19 10:38:28 +0530

    feat: adding models for peroson profiles

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,12 @@
 from .entity_payload_incoming import ENTITY_PAYLOAD
 from .attribute_payload_incoming import ATTRIBUTE_PAYLOAD
 from .write_payload import WRITE_PAYLOAD
+from .person_schemas import PersonSource, PersonResponse
 
 __all__ = [
     "ENTITY_PAYLOAD",
     "ATTRIBUTE_PAYLOAD",
     "WRITE_PAYLOAD",
+    "PersonSource",
+    "PersonResponse"
 ]
---
+++ src/models/person_schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+from datetime import date
+
+class PersonSource(BaseModel):
+    """
+    Person source schema
+    """
+    name: str
+    political_party: str
+    date_of_birth: date
+    religion: str
+    profession: str
+    email: str
+    phone_number: str
+    education_qualifications: str
+    professional_qualifications: str
+    image_URL: str
+
+class PersonResponse(PersonSource):
+    """
+    Person response schema
+    """
+    age: int
+    active_portfolio: str | None = None
